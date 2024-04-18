446a01e2fd8dd083751b9da5fd16ca73ac9c96fc
Jakob Eriksson <jkberiksson@gmail.com>
2024-04-18 16:18:03 +0200

    Added functionallity for blackjack game. Also added linters.

---
+++ src/Blackjack/Blackjack.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Blackjack;
+
+use App\Card\DeckOfCards;
+
+class Blackjack
+{
+    private $deck;
+    private $playerHand;
+    private $dealerHand;
+
+    public function __construct()
+    {
+        $this->deck = new DeckOfCards();
+        $this->playerHand = [];
+        $this->dealerHand = [];
+    }
+
+    public function startGame(): void
+    {
+        $this->deck->shuffleCards();
+        $this->dealCardToPlayer();
+        $this->dealCardToDealer();
+        $this->dealCardToPlayer();
+    }
+
+    public function getPlayerHand(): array
+    {
+        return $this->playerHand;
+    }
+
+    public function getDealerHand(): array
+    {
+        return $this->dealerHand;
+    }
+
+    public function hitPlayer(): void
+    {
+        $this->dealCardToPlayer();
+    }
+
+    public function hitDealer(): void
+    {
+        $this->dealCardToDealer();
+    }
+
+    private function dealCardToPlayer(): void
+    {
+        $this->playerHand[] = $this->deck->drawCard();
+    }
+
+    private function dealCardToDealer(): void
+    {
+        $this->dealerHand[] = $this->deck->drawCard();
+    }
+
+    public function calculateHandValue(array $hand): int
+    {
+        $value = 0;
+        $aceCount = 0;
+
+        foreach ($hand as $card) {
+            $rank = $card->getRank();
+            if ($rank === 'A') {
+                $aceCount++;
+            }
+            $value += $this->getCardValue($rank);
+        }
+
+        while ($aceCount > 0 && $value > 21) {
+            $value -= 10;
+            $aceCount--;
+        }
+
+        return $value;
+    }
+
+    private function getCardValue(string $rank): int
+    {
+        if (in_array($rank, ['J', 'Q', 'K'])) {
+            return 10;
+        } elseif ($rank === 'A') {
+            return 11;
+        } else {
+            return intval($rank);
+        }
+    }
+
+    public function calculateWinner(int $playerHandValue, int $dealerHandValue): string
+    {
+        if ($playerHandValue > 21) {
+            return "Du har blivit tjock. Dealern vann.";
+        }
+
+        if ($dealerHandValue > 21) {
+            return "Dealern har blivit tjock. Du vann.";
+        }
+
+        if ($playerHandValue == $dealerHandValue) {
+            return "Det blev oavgjort. Ingen vann.";
+        }
+
+        if ($playerHandValue > $dealerHandValue) {
+            return "Du har högre poäng än dealern. Du vann.";
+        }
+
+        return "Dealern har högre poäng än dig. Dealern vann.";
+    }
+}
---
+++ src/Controller/GameController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Controller;
+
+use App\Blackjack\Blackjack;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class GameController extends AbstractController
+{
+    #[Route('/game', name: "game")]
+    public function game(SessionInterface $session): Response
+    {
+        $session->clear();
+
+        $blackjack = new Blackjack();
+        $blackjack->startGame();
+        $session->set('blackjack', $blackjack);
+
+        return $this->render('game.html.twig');
+    }
+
+    #[Route('/game/play', name: "game/play")]
+    public function gamePlay(SessionInterface $session): Response
+    {
+        $blackjack = $session->get('blackjack');
+
+        $playerHand = $blackjack->getPlayerHand();
+        $dealerHand = $blackjack->getDealerHand();
+
+        $playerHandValue = $blackjack->calculateHandValue($playerHand);
+        $dealerHandValue = $blackjack->calculateHandValue($dealerHand);
+
+        if ($playerHandValue > 21 || $dealerHandValue >= 17) {
+            $winnerMsg = $blackjack->calculateWinner($playerHandValue, $dealerHandValue);
+            $session->set("winner", $winnerMsg);
+            $this->addFlash('success', $winnerMsg);
+        }
+
+        $data = [
+            'playerHand' => $playerHand,
+            'dealerHand' => $dealerHand,
+            'playerHandValue' => $playerHandValue,
+            'dealerHandValue' => $dealerHandValue,
+        ];
+
+        return $this->render('game-play.html.twig', $data);
+    }
+
+    #[Route('/game/play/post', name: "game/play/post", methods: ["POST"])]
+    public function gamePlayPost(Request $request, SessionInterface $session): Response
+    {
+        $blackjack = $session->get('blackjack');
+
+        $action = $request->request->get('action');
+
+        $dealerHand = $blackjack->getDealerHand();
+        $dealerHandValue = $blackjack->calculateHandValue($dealerHand);
+
+        switch ($action) {
+            case 'hit':
+                $blackjack->hitPlayer();
+                break;
+            case 'stand':
+                while ($dealerHandValue < 17) {
+                    $blackjack->hitDealer();
+                    $dealerHand = $blackjack->getDealerHand();
+                    $dealerHandValue = $blackjack->calculateHandValue($dealerHand);
+                }
+                break;
+            case 'restart':
+                $session->clear();
+                $blackjack = new Blackjack();
+                $blackjack->startGame();
+                $session->set('blackjack', $blackjack);
+                return $this->redirectToRoute('game/play');
+        }
+
+        $session->set('blackjack', $blackjack);
+
+        return $this->redirectToRoute('game/play');
+    }
+}
