90fce22f6690642ec0d0f7489a59ed86b59377d3
Jakob Eriksson <jkberiksson@gmail.com>
2024-05-23 15:12:33 +0200

    added crud for books

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -8,36 +8,82 @@ use App\Repository\BookRepository;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+
 
 class BookController extends AbstractController
 {
     #[Route('/library', name: 'library')]
-    public function viewAllBooks(
-        BookRepository $BookRepository
-    ): Response {
+    public function viewAllBooks(BookRepository $BookRepository): Response
+    {
         $books = $BookRepository->findAll();
 
-        $data = [
-            'books' => $books
-        ];
+        $data = ['books' => $books];
 
         return $this->render('book/index.html.twig', $data);
     }
 
     #[Route('/book/create', name: 'book_create', methods: ["POST"])]
-    public function createBook(
-        ManagerRegistry $doctrine
-    ): Response {
+    public function createBook(Request $request, ManagerRegistry $doctrine): Response
+    {
         $entityManager = $doctrine->getManager();
 
         $book = new Book();
-        $book->setTitle("hej");
-        $book->setIsbn("hej");
-        $book->setAuthor("hej");
-        $book->setImage("hej");
+        $book->setTitle($request->request->get('title'));
+        $book->setAuthor($request->request->get('author'));
+        $book->setIsbn($request->request->get('isbn'));
+        $book->setImage($request->request->get('image'));
 
         $entityManager->persist($book);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('library');
+    }
+
+    #[Route('/book/delete/{id}', name: 'book_delete', methods: ["POST"])]
+    public function deleteBook(ManagerRegistry $doctrine, int $id): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $book = $entityManager->getRepository(Book::class)->find($id);
+
+        $entityManager->remove($book);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('library');
+    }
+
+    #[Route('/library/{id}', name: 'book_by_id')]
+    public function showBookById(bookRepository $bookRepository, int $id): Response
+    {
+        $book = $bookRepository->find($id);
+
+        $data = ['book' => $book];
+
+        return $this->render('book/book.html.twig', $data);
+    }
+
+    #[Route('/library/update/{id}', name: 'show_book_update', methods: ["GET"])]
+    public function showUpdateBook(bookRepository $bookRepository, int $id): Response
+    {
+        $book = $bookRepository->find($id);
+
+        $data = ['book' => $book];
+
+        return $this->render('book/update.html.twig', $data);
+    }
+
+    #[Route('/book/update/{id}', name: 'book_update', methods: ["POST"])]
+    public function updateBook(ManagerRegistry $doctrine, Request $request, int $id): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $book = $entityManager->getRepository(Book::class)->find($id);
+
+        $book->setTitle($request->request->get('title'));
+        $book->setAuthor($request->request->get('author'));
+        $book->setIsbn($request->request->get('isbn'));
+        $book->setImage($request->request->get('image'));
+
         $entityManager->flush();
 
         return $this->redirectToRoute('library');
